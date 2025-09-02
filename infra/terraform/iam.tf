5cb18b5b1815660f6ce3c0f3fde7e3f299984095
GerardoSan <grbios_17@hotmail.com>
2025-09-02 16:21:17 -0500

    Update iam.tf

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -1,9 +1,11 @@
+# IAM Role para Lambda
 resource "aws_iam_role" "lambda_role" {
   name = "${var.project}-lambda-role"
 
   assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
 }
 
+# Trust policy: Lambda puede asumir este rol
 data "aws_iam_policy_document" "lambda_assume" {
   statement {
     effect = "Allow"
@@ -15,12 +17,15 @@ data "aws_iam_policy_document" "lambda_assume" {
   }
 }
 
-# Attach policies: CloudWatch Logs & SecretsManager read
+# --- Políticas adjuntas ---
+
+# 1. Logs en CloudWatch (policy AWS administrada)
 resource "aws_iam_role_policy_attachment" "cw_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# 2. Lectura de Secrets Manager
 resource "aws_iam_policy" "secrets_read_policy" {
   name   = "${var.project}-secrets-read"
   policy = jsonencode({
@@ -38,5 +43,31 @@ resource "aws_iam_policy" "secrets_read_policy" {
 resource "aws_iam_role_policy_attachment" "secrets_read_attach" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.secrets_read_policy.arn
+}
+
+# 3. Acceso a ENIs en VPC (necesario si Lambda usa vpc_config)
+resource "aws_iam_policy" "lambda_vpc_access" {
+  name   = "${var.project}-lambda-vpc-access"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:UnassignPrivateIpAddresses"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_vpc_access.arn
 }
 
