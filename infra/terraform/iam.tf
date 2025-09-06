5a5eefe148d4a16b83f4934d4fdb0c99794a8fd2
gerardo <grbios_17@hotmail.com>
2025-09-05 23:46:33 -0500

    fv4

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -45,7 +45,64 @@ resource "aws_iam_role_policy_attachment" "secrets_read_attach" {
   policy_arn = aws_iam_policy.secrets_read_policy.arn
 }
 
-# 3. Acceso a ENIs en VPC (necesario si Lambda usa vpc_config)
+# 3. ECS Task Execution Role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach the AmazonECSTaskExecutionRolePolicy to the role
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Allow ECS to pull images from ECR
+resource "aws_iam_policy" "ecr_pull_policy" {
+  name        = "${var.project}-ecr-pull-policy"
+  description = "Policy to allow ECS to pull images from ECR"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+        Resource = ["*"]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = ["*"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_pull_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecr_pull_policy.arn
+}
+
+# 4. Acceso a ENIs en VPC (necesario si Lambda usa vpc_config)
 resource "aws_iam_policy" "lambda_vpc_access" {
   name   = "${var.project}-lambda-vpc-access"
   policy = jsonencode({
