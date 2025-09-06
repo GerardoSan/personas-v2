23dc4d9d3fc3309c7cc0a988bb67d89cd7c70c2b
gerardo <grbios_17@hotmail.com>
2025-09-05 23:56:17 -0500

    fv5

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -45,31 +45,7 @@ resource "aws_iam_role_policy_attachment" "secrets_read_attach" {
   policy_arn = aws_iam_policy.secrets_read_policy.arn
 }
 
-# 3. ECS Task Execution Role
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "${var.project}-ecs-task-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Attach the AmazonECSTaskExecutionRolePolicy to the role
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# Allow ECS to pull images from ECR
+# ECR Pull Policy for ECS Task Execution Role
 resource "aws_iam_policy" "ecr_pull_policy" {
   name        = "${var.project}-ecr-pull-policy"
   description = "Policy to allow ECS to pull images from ECR"
@@ -97,7 +73,8 @@ resource "aws_iam_policy" "ecr_pull_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecr_pull_attach" {
+# Attach ECR pull policy to the existing ECS Task Execution Role
+resource "aws_iam_role_policy_attachment" "ecs_task_ecr_pull" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.ecr_pull_policy.arn
 }
